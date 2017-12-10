6c2523897106fad1cda7191ad57389682278c144
souzag32 <souzag32@gmail.com>
2017-12-10 12:22:56 -0200

    Arquivos iniciais do projeto

---
+++ src/main/java/com/restprogram/RestProgramApplication.java
@@ -0,0 +1,12 @@
+package com.restprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestProgramApplication.class, args);
+	}
+}
